f0c31b191ed99e11399f6f40ac84f145462d1116
Michael LaCroix <michael@lacroixdesign.net>
2016-06-30 19:57:56 +0700

    Remove main.js from src

--- webpack/dist.babel.js
+++ webpack/dist.babel.js
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 import config from './config.babel';
 
 config.plugins = config.plugins.concat([
-  new webpack.DefinePlugin({'process.env': {NODE_ENV: 'production'}}),
+  new webpack.DefinePlugin({ 'process.env': { NODE_ENV: 'production' } }),
   new webpack.optimize.DedupePlugin(),
   new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.optimize.UglifyJsPlugin(),
