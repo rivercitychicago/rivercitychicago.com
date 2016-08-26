5142d72873f5a4b71757812532ee5b9696773e0e
Michael LaCroix <michael@lacroixdesign.net>
2016-08-26 12:02:17 -0500

    Switch to official Squarespace Dev Server

---
+++ README.md
@@ -0,0 +1,30 @@
+# River City
+
+## Installation
+
+- Install dependencies with `npm install`.
+- Install the [Squarespace Local Development Server](https://developers.squarespace.com/local-development/).
+
+## Usage
+
+### Dev Server
+
+Run the Squarespace development server:
+
+```sh
+squarespace-server https://rivercitychicago.squarespace.com
+```
+
+### Assets
+
+Compile and watch assets for development:
+
+```sh
+npm start
+```
+
+To build assets for production:
+
+```sh
+npm run build
+```
--- webpack/watch.babel.js
+++ webpack/watch.babel.js
@@ -1,28 +1,28 @@
 import webpack from 'webpack';
 import config from './config.babel';
-// import BrowserSyncPlugin from 'browser-sync-webpack-plugin';
+import BrowserSyncPlugin from 'browser-sync-webpack-plugin';
 
 config.watch = true;
 config.cache = true;
-config.debug = true;
-config.devtool = 'eval';
+config.debug = false;
+config.devtool = null;
+// config.devtool = 'eval';
 // config.devtool = 'source-map';
 // config.sourceMapFilename = '[file].map';
 
 config.plugins = config.plugins.concat([
-  new webpack.DefinePlugin({'process.env': {NODE_ENV: 'development'}}),
-  // new BrowserSyncPlugin({
-  //   proxy: process.env.BROWSERSYNC_PROXY || 'localhost:5050',
-  //   host: process.env.BROWSERSYNC_HOST || '127.0.0.1',
-  //   port: process.env.BROWSERSYNC_PORT || 5000,
-  //   logConnections: true,
-  //   open: false,
-  //   files: [
-  //     'styles/*.css',
-  //   ],
-  // }, {
-  //   reload: false,
-  // }),
+  new webpack.DefinePlugin({ 'process.env': { NODE_ENV: 'production' } }),
+  new webpack.optimize.UglifyJsPlugin(),
+  new BrowserSyncPlugin({
+    proxy: process.env.BROWSERSYNC_PROXY || 'localhost:9000',
+    host: process.env.BROWSERSYNC_HOST || 'localhost',
+    port: process.env.BROWSERSYNC_PORT || 5000,
+    logConnections: true,
+    open: false,
+    files: [
+      'styles/*.css',
+    ],
+  }),
 ]);
 
 export default config;
